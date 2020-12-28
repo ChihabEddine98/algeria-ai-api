2132468ac32f3a608def677bc93d1065bf5c2d78
Chihab Eddine <ga_benamara@esi.dz>
2020-12-29 00:56:51 +0100

    First mail sending test

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from typing import Optional
-
+import os
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
+from starlette.responses import JSONResponse
+from starlette.requests import Request
+
+from fastapi_mail import ConnectionConfig,FastMail, MessageSchema
 
 app = FastAPI()
 
@@ -12,6 +16,31 @@ class Email(BaseModel):
     email: EmailStr
     message : str
 
+conf = ConnectionConfig(
+    MAIL_PASSWORD = os.environ['MAIL_PWD'],
+    MAIL_FROM = "tiaret.shopper@gmail.com",
+    MAIL_PORT = 587,
+    MAIL_SERVER = "smtp.gmail.com",
+    MAIL_TLS = True,
+    MAIL_SSL = False
+)
+template = """
+<p>Hi this test mail using BackgroundTasks, thanks for using Fastapi-mail</p> 
+"""
+
+@app.get("/send_mail")
+async def simple_send() -> JSONResponse:
+
+    message = MessageSchema(
+        subject="Fastapi-Mail module",
+        recipients=['chihabeddineben@gmail.com'],  # List of recipients, as many as you can pass
+        body=template,
+        subtype="html"
+        )
+
+    fm = FastMail(conf)
+    await fm.send_message(message)
+    return JSONResponse(status_code=200, content={"message": "email has been sent"})
 
 @app.get("/")
 def read_root():
